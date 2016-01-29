ba96c2be6808eae7a0182f9e622d68f375965325
Dennis Bailey <bailey.dennis@gmail.com>
2016-01-28 20:35:05 -0700

    Mockup of dashboard

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,45 @@
 // add scripts
+$(document).ready(function(){
+  console.log("READY, PLAYER ONE?");
+  
+  var ipRequest = {
+    "url": "http://jsonip.com/",
+    "method": "GET"
+  }
+    
+  $.ajax(ipRequest).done(function(ipResponse) {
+    console.log(ipResponse);
+    var ipAddress = ipResponse.ip;
+    
+    var locationRequest = {
+    "url": "http://freegeoip.net/json/" + ipAddress,
+    "method": "GET"
+    }
+    
+    $('#ip').append(ipAddress); 
+    
+    $.ajax(locationRequest).done(function(locationResponse) {
+    console.log(locationResponse);
+    var latitude = locationResponse.latitude;
+    var longitude = locationResponse.longitude;
+    console.log("this is the latitude: " + latitude);
 
-$(document).on('ready', function() {
-  console.log('sanity check!');
+      var forecast = {
+        "url": "https://api.forecast.io/forecast/1f20d4933aaab481a0b34eb3559b6d50/" + latitude + "," + longitude,
+        "method": "GET",
+        "dataType": "jsonp"
+      }
+      
+      console.log(forecast.url);
+      
+      $.ajax(forecast).done(function (forecastResponse) {
+        var currentWeather = forecastResponse.currently.summary
+        var dailyWeather = forecastResponse.daily.summary
+
+        $('#weather-now').append(currentWeather);
+        $('#weather-summary').append(dailyWeather);
+        
+      });                    
+    });  
+  });
 });
